be60e4f136a6a22a87de92c03c2b59b68aeb14d7
AfnanMac <amldn.afnn@gmail.com>
2025-03-04 13:44:16 +0700

    Update: CRUD USER

--- authentication/apis/serializers/user.py
+++ authentication/apis/serializers/user.py
@@ -4,4 +4,21 @@ from authentication.models.user import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['user_id', 'user_name', 'real_name', 'email', 'phone']
+        fields = [
+            'user_id', 
+            'user_name', 
+            'real_name', 
+            'email', 
+            'phone',
+            'password',
+            'avatar',
+            'locale',
+            'timezone',
+            'last_ip_address',
+            'last_activity_date',
+            'actived_date',
+            'is_staff',
+            'is_active',
+            'date_joined',
+            'status'
+        ]
--- authentication/apis/views/user.py
+++ authentication/apis/views/user.py
@@ -1,9 +1,9 @@
 import logging
-from django.contrib.auth.mixins import LoginRequiredMixin
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from authentication.models.user import User
 from authentication.apis.serializers import UserSerializer
+from rest_framework.response import Response
 
 logger = logging.getLogger(__name__)
 
@@ -11,3 +11,36 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            print('AKU MASOKKKK')
+            return Response(
+                {"message": "User created successfully!", "data": serializer.data},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {"message": "Failed to create user", "errors": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+        
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User updated successfully!", "data": serializer.data},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            {"message": "Failed to update user", "errors": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
